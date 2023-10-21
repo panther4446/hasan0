21872727a68fd90ab459afb36a4c1dbcb46f565c
panther4446 <67631928+panther4446@users.noreply.github.com>
2023-10-21 11:41:52 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,196 +1,51 @@
-// var City  = prompt("ENTER YOUR CITY");
-// if(City==="karachi"){
-// } 
-//    else{
-//         document.write("Wellcome To City Of Light");
-//     }
-
-
-// var Gender = prompt("ENTER YOUR GENDER")
-
-// if( Gender === "Male"){
-
-// }else {
-//     document.write("GOOD MORNING SIR ");
-// }
-
-// if(
-//     Gender === "Female"
-// ){
-
-// } if{
-//     document.write("GOOD MORNING MAM")
-//  } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var carfuel= +prompt("how many liters")
-// if ( carfuel <=  5 )
-// document.write("Please refill your fuel in the car")
-
-
-
-// var a=4;
-// if (++a===5){
-//     alert("given condition for variable a is true")
-
-// }
-// var b=4;
-// if (++b===5){
-//     alert("given condition for variable b is true")
-
-// }
-// var c=4;
-// if (++a===5){
-//     alert("condition 1 is true")
-// }
-// if(c===13){
-//     alert("condition 2 is true ")
-// }
-// if (++c<14){
-//     alert("condition 3 is true")
-// }
-// if (===14){
-//     alert("condition 4 is true")
-// }
-
-//  var materialCost = 20000
-//  var laborCost=2000
-//  var totalCost = materialCost + laborCost;
-//  if(totalCost === laborCost + materialCost){
-//     alert ("the cost equal")
-//  }
-
-
-// if (true){
-//     alert("true")
-// alert("false");
-// }
-
-// if ("car" <"cat"){
-//     alert("cat is smaller than cat");
-// }
-
-
-// var firstsubject = +prompt("Enter firstsubject marks");
-// var secondsubject = +prompt("Enter secondsubject marks");
-// var thirdsubject = +prompt("Enter thirdsubject marks");
-// var totalMarks = +prompt("Enter totalsubject marks");
-
-
-// var obtainMarks = firstsubject + secondsubject + thirdsubject;
-// var percentage = (obtainMarks / totalMarks ) * 100;
-
-
-
-// if(percentage >-80 && percentage <-100)
-// {
- 
-//     document.write("percentage:" + percentage + "<br>" + "Grade:" + "A+" +   "<br>" +   "Remarks" + "excellent" )
-// }
-
-// else if(percentage >=70 && percentage <=80)
-// {
- 
-//     document.write("percentage:" + percentage + "<br>" + "Grade:" + "A" + "<br>" + "Remarks" + "Good"   )
-// }
-
-//  else if(percentage >=60 && percentage <=70)
-// {
- 
-//     document.write("percentage:" + percentage + "<br>" + "Grade:" + "B" + "<br>" + "Remarks" + "you need to improve"   )
-// }
-
-//  else if(percentage >=50 && percentage <=40)
-// {
- 
-    // document.write("percentage:" + percentage + "<br>" + "Grade:" + "C" + "<br>" + "Remarks " + " Sorry "   )
-// }
-
-
-
-
-// var num = +prompt("Guess the number ");
-
-// if (num=== 5)
-// {
-//     document.write("bingo!")
-// }
-// else if ( num ==2){
-//     document.write("Close to enough to the correct  Anwser ")
-// }
-
-
-
-
-
-
-
-
-// var num1 = +prompt("enter first value");
-// var operator = prompt("enter your operator");
-// var num2 = +prompt("Enter second value");
-
-// if (operator === "+") {
-//   alert(num1 + num2);
-// } else if (operator === "-") {
-//   alert(num1 - num2);
-// } else if (operator === "*") {
-//   alert(num1 * num2);
-// } else if (operator === "/") {
-//   alert(num1 / num2);
-// }
-
-//  var password = +prompt("enter your password")
-//  if (password == "javascipt ")
-//  document.write(  password +"password is corrent")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var hourHeading = document.getElementById("hour");
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+var hour = 0 ;
+var min = 0 ;
+var sec = 0 ;
+var msec = 0;
+var interval ;
+
+
+function timer(){
+    msec++;
+    msecHeading.innerHTML = msec;
+    if (msec >=100){ 
+      sec++;
+      secHeading.innerHTML = sec;
+      msec = 0;  
+    }else if (sec >= 60){
+       min++;
+       MinHeading.innerHTML = min;
+       sec = 0;
+    }else if (min >= 60){
+        hour++;
+        hourHeading.innerHTML = hour;
+        min = 0;
+    }
+}
+
+function start(){
+    var start = document.getElementById("startbtn")
+    interval = setInterval(timer, 10)
+    start.disabled = true;
+}
+function pause(){
+    var start = document.getElementById("startbtn")
+    clearInterval(interval);
+    start.disabled = false;
+}
+function reset(){
+    var hour = 0;
+    var min = 0;
+    var sec = 0;
+    var msec = 0;
+msecHeading.innerHTML = msec;
+minHeading.innerHTML = min;
+hourHeading.innerHTML = hour;
+secHeading.innerHTML = sec;
+pause();
+}
 
