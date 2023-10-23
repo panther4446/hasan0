76eb2426e12f377f670b492af930c9d84048370d
panther4446 <67631928+panther4446@users.noreply.github.com>
2023-10-23 04:59:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
-var hourHeading = document.getElementById("hour");
-var minHeading = document.getElementById("min");
-var secHeading = document.getElementById("sec");
-var msecHeading = document.getElementById("msec");
-var hour = 0 ;
-var min = 0 ;
-var sec = 0 ;
-var msec = 0;
-var interval ;
-
-
-function timer(){
-    msec++;
-    msecHeading.innerHTML = msec;
-    if (msec >=100){ 
-      sec++;
-      secHeading.innerHTML = sec;
-      msec = 0;  
-    }else if (sec >= 60){
-       min++;
-       MinHeading.innerHTML = min;
-       sec = 0;
-    }else if (min >= 60){
-        hour++;
-        hourHeading.innerHTML = hour;
-        min = 0;
-    }
-}
-
-function start(){
-    var start = document.getElementById("startbtn")
-    interval = setInterval(timer, 10)
-    start.disabled = true;
-}
-function pause(){
-    var start = document.getElementById("startbtn")
-    clearInterval(interval);
-    start.disabled = false;
-}
-function reset(){
-    var hour = 0;
-    var min = 0;
-    var sec = 0;
-    var msec = 0;
-msecHeading.innerHTML = msec;
-minHeading.innerHTML = min;
-hourHeading.innerHTML = hour;
-secHeading.innerHTML = sec;
-pause();
-}
-
+function addtodo() {
+    var input = document.getElementById("inputField");
+    //   console.log(input.value);
+  
+    // ***************************Create list*********************************
+  
+    var liElement = document.createElement("li");
+  
+    var liText = document.createTextNode(input.value);
+  
+    liElement.appendChild(liText);
+  
+    // ***********************Delete button**********************************
+  
+    var delbtn = document.createElement("button");
+  
+    var delbtnText = document.createTextNode("Delete");
+  
+    delbtn.appendChild(delbtnText);
+  
+    delbtn.setAttribute("onclick", "deleteItem(this)");
+  
+    liElement.appendChild(delbtn);
+  
+    // ***********************Edit button**********************************
+  
+    var editbtn = document.createElement("button");
+  
+    var editbtnText = document.createTextNode("Edit");
+  
+    editbtn.appendChild(editbtnText);
+  
+    liElement.appendChild(editbtn);
+  
+    editbtn.setAttribute("onclick", "editItem(this)");
+  
+    var list = document.getElementById("list");
+  
+    list.appendChild(liElement);
+  
+    console.log(liElement);
+  
+    input.value = "";
+  }
+  
+  function deleteAll() {
+    var list = document.getElementById("list");
+    list.innerHTML = "";
+  }
+  
+  function deleteItem(x) {
+    console.log(x.parentNode.remove());
+  }
+  
+  function editItem(e) {
+    var input = prompt("Enter updated value...");
+  
+    e.parentNode.firstChild.nodeValue = input;
+  }
